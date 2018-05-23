e729f1bc0496907e14d3c0068e8062db0bf230f4
Joshua McNabb <791799@ACDM.DS.SAIT.CA>
2018-05-23 10:29:17 -0600

    Module 9.

---
+++ inc/linkvariables.php
@@ -0,0 +1,8 @@
+<?php
+    $travelWebsites = array(
+                          'https://www.expedia.ca/' => 'Expedia',
+                          'https://www.trivago.ca' => 'Trivago',
+                          'https://www.hotwire.com/' => 'Hotwire',
+                          'https://www.ca.kayak.com/' => 'Kayak',
+                          'https://www.travelocity.ca/' => 'Travelocity'
+                      );
--- links.php
+++ links.php
@@ -1,9 +1,10 @@
 <?php
+    include('inc/linkvariables.php');
+
     echo '<table>';
 
-    for($i = 1; $i <= 6; $i++) {
-        echo '<tr><td>' . $i . ' - </td>
-              <td><a href="pages/page' . $i . '.php">Page ' . $i . '</a></td><tr>';
+    foreach ($travelWebsites as $url => $name) {
+        echo '<tr><td><a href="' . $url . '" target="_blank">' . $name . '</a></tr></td>';
     }
 
     echo '</table>';
