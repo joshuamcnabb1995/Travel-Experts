4c37427744b637700ba57fef716f254ac3be4f7e
Joshua McNabb <791799@ACDM.DS.SAIT.CA>
2018-05-24 08:09:49 -0600

    Add page title.

--- links.php
+++ links.php
@@ -1,10 +1,29 @@
-<?php
-    include('inc/linkvariables.php');
+<?php $page = 7; ?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <?php include('inc/meta.php'); ?>
 
-    echo '<table>';
+        <title>Travel Experts - Links</title>
 
-    foreach ($travelWebsites as $url => $name) {
-        echo '<tr><td><a href="' . $url . '" target="_blank">' . $name . '</a></tr></td>';
-    }
+        <?php include('inc/css.php'); ?>
+    </head>
 
-    echo '</table>';
+    <body>
+        <?php
+            include('inc/nav.php');
+            include('inc/header.php');
+            include('inc/linkvariables.php');
+
+            echo '<table>';
+
+            foreach($travelWebsites as $url => $name) {
+                echo '<tr><td><a href="' . $url . '" target="_blank">' . $name . '</a></tr></td>';
+            }
+
+            echo '</table>';
+
+            include('inc/footer.php');
+        ?>
+    </body>
+</html>
