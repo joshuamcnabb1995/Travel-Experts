4ed69d296fcdde1375c622c2e022695df4120ac5
Joshua McNabb <791799@ACDM.DS.SAIT.CA>
2018-05-24 08:09:55 -0600

    .

--- functions/getdate.php
+++ functions/getdate.php
@@ -3,18 +3,18 @@
 
     function displayWelcomeMessage()
     {
-        $date = date('Gis'); // Get the 24 hour time in hours, minutes and seconds
+        $date = date('H'); // Get the 24 hour time in hours
 
-        // Between 5:00:01 and noon say good morning
-        if($date > 50000 && $date < 120000)
+        // Between 5:00 and noon say good morning
+        if($date > 5 && $date < 12)
             return 'Good Morning';
 
-        // Between 12:00:01 and 6:00pm say good afternoon
-        else if($date > 120000 && $date <= 180000)
+        // Between 12:00 and 6:00pm say good afternoon
+        else if($date > 12 && $date <= 18)
             return 'Good Afternoon';
 
-        // Between 6:00:01pm and 10:00pm say good evening
-        else if($date > 180000 && $date < 220000)
+        // Between 6:00pm and 10:00pm say good evening
+        else if($date > 18 && $date < 22)
             return 'Good Evening';
 
         // Between 10:00:01pm and 4:59am
--- inc/nav.php
+++ inc/nav.php
@@ -1,3 +1,4 @@
+<?php if(!isset($page)) $page = 0; // If the page variable isn't set, make the home the active page rather than throwing an error ?>
 <nav>
     <div id="myTopnav" class="topnav">
         <a id="logo" href="<?php if($page == 0) echo '#'; else echo '/'; ?>"<?php if($page == 0) echo ' class="active"'; ?>>Travel Experts</a>
@@ -6,7 +7,8 @@
         <a href="#"<?php if($page == 3) echo ' class="active"'; ?>><i class="fa fa-car"></i>&nbsp; Car Rentals</a>
         <a href="#"<?php if($page == 4) echo ' class="active"'; ?>><i class="fa fa-plane"></i>&nbsp; Flights</a>
         <a href="#"<?php if($page == 5) echo ' class="active"'; ?>><i class="fa fa-suitcase"></i>&nbsp; Vacations</a>
-        <a href="contact/index.html"<?php if($page == 15) echo ' class="active"'; ?>><i class="fa fa-pencil"></i>&nbsp; Contact Us</a>
+        <a href="contact/index.php"<?php if($page == 6) echo ' class="active"'; ?>><i class="fa fa-pencil"></i>&nbsp; Contact Us</a>
+        <a href="<?php if($page == 7) echo '#'; else echo '/links.php'; ?>"<?php if($page == 7) echo ' class="active"'; ?>><i class="fa fa-link"></i>&nbsp; Links</a>
         <div class="right">
             <a href="<?php if($page == 16) echo '#'; else echo '/register'; ?>"<?php if($page == 16) echo ' class="active"'; ?>><i class="fa fa-user-plus"></i>&nbsp; Register</a>
             <a href="#"<?php if($page == 17) echo ' class="active"'; ?>><i class="fa fa-lock"></i>&nbsp; Login</a>
