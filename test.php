304948223114310e20279d267642ea552b6f9a9e
joshuam1995 <joshuam1995@users.noreply.github.com>
2018-05-25 18:50:59 -0600

    Change filename.

--- test.php
+++ test.php
@@ -1,10 +1,16 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>PHP Test</title>
-    </head>
+<?php
+    include('functions/functions.php');
 
-    <body>
-        <?php echo 'Hello, world!'; ?>
-    </body>
-</html>
+    // Whatever data you want to insert into the table
+    $insertData = array('AgtFirstName' => 'Joshua',
+                        'AgtMiddleInitial' => 'A',
+                        'AgtLastName' => 'McNabb',
+                        'AgtBusPhone' => 12345678901,
+                        'AgtEmail' => 'joshua.mcnabb@edu.sait.ca',
+                        'AgtPosition' => 'President',
+                        'AgencyId' => 1);
+
+    if(addNewTableResult('agents', $insertData))
+        echo 'A new result was successfully inserted into the agents table.';
+    else
+        echo 'The result wasn\'t inserted into the agents table.';
